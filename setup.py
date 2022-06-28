a308ebc182f5bdba6682b7a20e8e5fb7941428d8
Chip Kent <5250374+chipkent@users.noreply.github.com>
2022-06-28 10:09:03 -0600

    Created a demo python package

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = "0.0.1"
+
+setuptools.setup(
+    name="test_dhaal",
+    version=version,
+    author="David R. (Chip) Kent IV",
+    author_email="chipkent@deephaven.io",
+    description="Test Deephaven as a Library Python integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chipkent/test-dhaal",
+    project_urls={
+        "GitHub": "https://github.com/chipkent/test-dhaal",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "deephaven==0.14.0.dev5",
+    ],
+)
---
+++ test_scripy.py
@@ -0,0 +1,6 @@
+
+import test_dhaal
+
+test_dhaal.start_dh()
+
+t, t_last, t_join = test_dhaal.example_query()
